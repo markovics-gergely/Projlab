a76be447eccf1d846bc6dc4afa406d03faa8fec4
n0it <52136313+n0it@users.noreply.github.com>
2020-04-23 21:01:58 +0200

    Add files via upload

--- IceField/src/logic/Way.java
+++ IceField/src/logic/Way.java
@@ -1,35 +1,20 @@
 package logic;
 
 public enum Way {
-    up,         //felfele jobbra
+    up,
     right,
-    down,       //lefele jobbra
-    left,
-    upleft,     //felfele balra DE MINEK EZ HA ALAPBÓL CSAK 4 OLDALÚ AZ ALAP PÁLYA
-    downleft;   //lefele balra
+    down,
+    left;
 
-    /**
-     * Visszaadja az ellenkező irányt annak, amin meghívták.
-     * @return  Visszaadja az ellenkező irányt annak, amin meghívták.
-     */
     public Way opposite(){
         switch(this) {
             case up: return down;
             case right: return left;
             case down: return up;
             case left: return right;
-            case upleft: return downleft; //FÖLÖSLEGES CSAK TESZTRE KELL MERT 6 OLDALÚ A TESZT MIKÖZBEN NEM FELADAT
-            case downleft: return upleft; //FÖLÖSLEGES CSAK TESZTRE KELL MERT 6 OLDALÚ A TESZT MIKÖZBEN NEM FELADAT
             default: throw new IllegalStateException("Opposite Error");
         }
     }
-
-    /**
-     * Elforgatja 90 fokkal az irányt amin
-     * meghívták, a paraméter szerinti irányba.
-     * @param toRight jobbra vagy balra
-     * @return irány
-     */
     public Way rotate(boolean toRight){
         switch(this) {
             case up: return toRight ? right : left;
